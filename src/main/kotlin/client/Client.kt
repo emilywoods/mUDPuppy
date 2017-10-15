25c3a7ea14772d26f295c0cf47fa66899878c549
Emily Woods <emilymonica.woods@gmail.com>
2017-10-15 13:17:15 +0100

    Use dataInputStream instead of scanner

--- src/main/kotlin/client/Client.kt
+++ src/main/kotlin/client/Client.kt
@@ -1,20 +1,16 @@
 package client
 
+import java.io.DataInputStream
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
-import java.util.*
 
 class Client() : Thread() {
     val bufferSize = 1024
     val udpPort = 4320
     val socket = DatagramSocket()
     val ipAddress = InetAddress.getLocalHost()
-    val packet = ByteArray(bufferSize)
-
-
-    val sendByte =  byteArrayOf()[bufferSize]
-    val receiveByte =  byteArrayOf()[bufferSize]
+    val receiveBytes = ByteArray(bufferSize)
 
     fun client() {
         while (true) {
@@ -25,23 +21,37 @@ class Client() : Thread() {
     }
 
     private fun send() {
-        val dataInputStream = Scanner(System.`in`)
-        println("Client:")
-        val sendString = dataInputStream.nextLine()
-        val sendByte  = sendString.toByteArray()
+//        val dataInputStream = Scanner(System.`in`)
+//        println("Client:")
+//        val sendString = dataInputStream.nextLine()
+//        var sendByte  = sendString.toByteArray()
+//        val sendPacket = DatagramPacket(sendByte, sendByte.size, ipAddress, udpPort)
+//        socket.send(sendPacket)
+//        println("Message sent")
+
+
+        val dataInputStream = DataInputStream(System.`in`)
+        print("Client:")
+        val sendStr = dataInputStream.readUTF()
+        val sendByte = sendStr.toByteArray()
         val sendPacket = DatagramPacket(sendByte, sendByte.size, ipAddress, udpPort)
         socket.send(sendPacket)
         println("Message sent")
 
-
     }
 
     private fun receive() {
-        val receivePacket = DatagramPacket(packet, packet.size)
+//        val receivePacket = DatagramPacket(receiveBytes, receiveBytes.size)
+//        socket.receive(receivePacket)
+//        println("Message received")
+//        val receiveStr = receivePacket.data.toString()
+//        println("Server:" + receiveStr)
+
+        val receivePacket = DatagramPacket(receiveBytes, receiveBytes.size)
         socket.receive(receivePacket)
-        println("Message received")
-        val receiveStr = receivePacket.data.toString()
-        println("Server:" + receiveStr)
+        val receiveStr = String(receivePacket.getData())
+        val receiveStrTrim = receiveStr.trim()
+        System.out.println("Server:"+ receiveStrTrim)
     }
 
 }
